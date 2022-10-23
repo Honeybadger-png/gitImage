43faa618a4d740483a3f99ed9d3aed649680e2dd
Honeybadger-png <9emertgul@gmail.com>
2022-10-23 12:26:41 +0300

    changes committed

--- firstweek.py
+++ firstweek.py
@@ -1,4 +1,17 @@
 # if statement
+
+
 number = 5
 if number <= 5:
     print("number is less than 5") 
+
+def getNumber():
+    n= int(input("how many number will be added"))
+    a=[]
+    for i in range(0,n):
+        elem=int(input("enter a number: "))
+        a.append(elem)
+    avg=sum(a)/n
+    print("average is :",round(avg,2))
+
+getNumber()
