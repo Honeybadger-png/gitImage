dd718336e4bf0533e796d41d55e0d95aa21b4f78
Honeybadger-png <9emertgul@gmail.com>
2022-11-13 18:54:44 +0300

    new week practice

---
+++ 6.week/blur.py
@@ -0,0 +1,20 @@
+
+# importing the modules needed
+import cv2
+import numpy as np
+  
+# Reading the image
+image = cv2.imread('toppng.com-free-vintage-human-brain-1920x1381.png')
+  
+# Creating the kernel(2d convolution matrix)
+kernel1 = np.ones((5, 5), np.float32)/30
+  
+# Applying the filter2D() function
+img = cv2.filter2D(src=image, ddepth=-1, kernel=kernel1)
+  
+# Shoeing the original and output image
+cv2.imshow('Original', image)
+cv2.imshow('Kernel Blur', img)
+  
+cv2.waitKey()
+cv2.destroyAllWindows()
